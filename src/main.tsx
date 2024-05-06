82d6344c048aa5dcdcce1f30cf1277baa5221ebc
Mannushka <zaimar1910@gmail.com>
2024-05-07 00:51:48 +0800

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RestaurantInfo from "./pages/restaurant/RestaurantInfo.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -19,7 +21,15 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           "read:current_user update:current_user_metadata openid profile email read:user_metadata",
       }}
     >
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route
+            path="/restaurants/:restaurantId"
+            element={<RestaurantInfo />}
+          />
+        </Routes>
+      </BrowserRouter>
     </Auth0Provider>
   </React.StrictMode>
 );

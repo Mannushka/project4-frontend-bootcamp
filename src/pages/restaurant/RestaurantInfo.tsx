cd020f188496e72f3074040f05d1d999485946d1
Mannushka <zaimar1910@gmail.com>
2024-05-19 18:46:07 +0800

    edit useEffect dependencies array

--- src/pages/restaurant/RestaurantInfo.tsx
+++ src/pages/restaurant/RestaurantInfo.tsx
@@ -37,7 +37,7 @@ const RestaurantInfo = () => {
       }
     };
     getRestaurantInfo();
-  }, [restaurantId]);
+  }, [updateRestaurantId, restaurantId]);
 
   // const displayRestaurantInfo = (): JSX.Element[] => {
   //   const infoElements = [];

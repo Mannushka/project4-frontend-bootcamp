0f6029f863958ed3b1be2e6a59b7c2cc3d164e74
Mannushka <zaimar1910@gmail.com>
2024-05-19 18:45:11 +0800

    save user id in local storage

--- src/context/UserInfoContext.tsx
+++ src/context/UserInfoContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 interface UserInfoProviderProps {
   children: ReactNode;
 }
@@ -15,12 +21,19 @@ export function useUserInfo() {
 }
 
 export function UserInfoProvider({ children }: UserInfoProviderProps) {
-  const [userId, setUserId] = useState<number>(0);
+  const storedUserId = JSON.parse(
+    localStorage.getItem("userId") || "0"
+  ) as number;
+
+  const [userId, setUserId] = useState<number>(storedUserId);
+
+  useEffect(() => {
+    localStorage.setItem("userId", JSON.stringify(userId));
+  }, [userId]);
 
   const updateUserId = (id: number): void => {
     setUserId(id);
   };
-
   return (
     <UserInfoContext.Provider
       value={{

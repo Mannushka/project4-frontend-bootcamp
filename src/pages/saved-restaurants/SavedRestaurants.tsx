b12f04a5fbc84be88f81354c12ff0dd208189480
Mannushka <zaimar1910@gmail.com>
2024-05-19 22:48:51 +0800

    create saved restaurants page

---
+++ src/pages/saved-restaurants/SavedRestaurants.tsx
@@ -0,0 +1,41 @@
+import { BACKEND_URL, USER } from "../../constants";
+import axios from "axios";
+import { useUserInfo } from "../../context/UserInfoContext";
+import { useState, useEffect } from "react";
+import { validateId } from "../../utils/validateId";
+import SingleRestaurantCard from "../../components/restaurants-listings/SingleRestaurantCard";
+const SavedRestaurants = () => {
+  const [savedRestaurants, setSavedRestaurants] = useState<Restaurant[]>([]);
+  const { userId } = useUserInfo();
+
+  useEffect(() => {
+    const getSavedRestaurants = async (): Promise<void> => {
+      try {
+        validateId(userId, USER);
+        const response = await axios.get(
+          `${BACKEND_URL}/users/${userId}/my-restaurants`
+        );
+
+        setSavedRestaurants(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+        throw new Error("An error occurred while fetching saved restaurants.");
+      }
+    };
+
+    getSavedRestaurants();
+  }, [userId, savedRestaurants]);
+
+  const savedRestaurantsList = savedRestaurants.map((restaurant) => {
+    return (
+      <div key={restaurant.id}>
+        <SingleRestaurantCard restaurant={restaurant} />
+      </div>
+    );
+  });
+
+  return <div>{savedRestaurantsList}</div>;
+};
+
+export default SavedRestaurants;

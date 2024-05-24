13387e3d69a56073bb18b343978baa159a186aee
Mannushka <zaimar1910@gmail.com>
2024-05-24 23:42:47 +0800

    create responsive navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,13 +4,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserMenu from "./UserMenu";
-
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// interface NavBarProps {
-//   brandName: string;
-//   imageSrcPath: string;
-//   navItems: string[];
-// }
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const NavBar = (): JSX.Element => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -34,140 +32,138 @@ const NavBar = (): JSX.Element => {
     }
   };
 
+  // return (
+  //   <div>
+  //     <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
+  //       <div className="container-fluid">
+  //         <a className="navbar-brand" href="#">
+  //           <img
+  //             src={imagePath}
+  //             width="55"
+  //             height="50"
+  //             className="d-inline-block align-center me-2"
+  //             alt="food_harbour"
+  //           />
+  //           <span className="fw-bolder fs-4"> {brandName}</span>
+  //         </a>
+  //         <button
+  //           className="navbar-toggler"
+  //           type="button"
+  //           data-toggle="collapse"
+  //           data-target="#navbarSupportedContent"
+  //           aria-controls="navbarSupportedContent"
+  //           aria-expanded="false"
+  //           aria-label="Toggle navigation"
+  //         >
+  //           <span className="navbar-toggler-icon"></span>
+  //         </button>
+  //         <div
+  //           className="collapse
+  //         navbar-collapse
+  //         align-items-start
+  //         d-flex
+  //         flex-column
+  //         flex-md-row"
+  //           id="navbarSupportedContent"
+  //         >
+  //           <ul className="navbar-nav me-auto mb-2 mb-lg-1">
+  //             {navItems.map((items, index) => (
+  //               <li
+  //                 key={items}
+  //                 className="nav-item"
+  //                 onClick={() => {
+  //                   setSelectedIndex(index);
+  //                   navigateToPage(items);
+  //                 }}
+  //               >
+  //                 <a
+  //                   className={
+  //                     selectedIndex === index
+  //                       ? "nav-link active fw-bold"
+  //                       : "nav-link"
+  //                   }
+  //                   // href="#"
+  //                 >
+  //                   {items}
+  //                 </a>
+  //               </li>
+  //             ))}
+  //           </ul>
+
+  //           <div className="me-4 mb-2 ">
+  //             {/* {isAuthenticated && !isLoading && `Hello ${user?.first_name}`} */}
+  //             {isAuthenticated && !isLoading && <UserMenu />}
+  //           </div>
+  //           <div className="me-4 mb-2">
+  //             {isAuthenticated && !isLoading && <LogoutButton />}
+  //             {!isAuthenticated && !isLoading && <LoginButton />}
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </nav>
+  //   </div>
+  // );
+
   return (
-    <div>
-      <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img
-              src={imagePath}
-              width="55"
-              height="50"
-              className="d-inline-block align-center me-2"
-              alt=""
-            />
-            <span className="fw-bolder fs-4"> {brandName}</span>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+    <Navbar expand="lg" className="bg-body-tertiary shadow">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img
+            src={imagePath}
+            width="55"
+            height="50"
+            className="d-inline-block align-center me-2"
+            alt="food_harbour"
+          />
+          <span className="fw-bolder fs-4"> {brandName}</span>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbarScroll" />
+
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse 
-          navbar-collapse 
-          align-items-start
-          d-flex
-          flex-column
-          flex-md-row"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav me-auto mb-2 mb-lg-1">
-              {navItems.map((items, index) => (
-                <li
-                  key={items}
-                  className="nav-item"
-                  onClick={() => {
-                    setSelectedIndex(index);
-                    navigateToPage(items);
-                  }}
-                >
-                  <a
-                    className={
-                      selectedIndex === index
-                        ? "nav-link active fw-bold"
-                        : "nav-link"
-                    }
-                    // href="#"
-                  >
-                    {items}
-                  </a>
-                </li>
-              ))}
-              {/* <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle me-5"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Restaurants
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </li> */}
-              {/* <li>
-                <form className=" d-flex me-3" role="search">
-                  <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                  <button
-                    className="btn btn-outline-success my-2 my-sm-0"
-                    type="submit"
-                  >
-                    Search
-                  </button>
-                </form>
-              </li> */}
-            </ul>
-            {/* <form className=" d-flex me-5 mb-2" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                type="submit"
+            {navItems.map((item, index) => (
+              <Nav.Link
+                key={item}
+                href={`#${item.toLowerCase()}`}
+                onClick={() => {
+                  setSelectedIndex(index);
+                  navigateToPage(item);
+                }}
+                className={selectedIndex === index ? "active fw-bold" : ""}
               >
-                Search
-              </button>
-            </form> */}
-            <div className="me-4 mb-2 ">
-              {/* {isAuthenticated && !isLoading && `Hello ${user?.first_name}`} */}
-              {isAuthenticated && !isLoading && <UserMenu />}
-            </div>
-            <div className="me-4 mb-2">
-              {isAuthenticated && !isLoading && <LogoutButton />}
-              {!isAuthenticated && !isLoading && <LoginButton />}
-            </div>
+                {item}
+              </Nav.Link>
+            ))}
+          </Nav>
+          {/* <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+
+          {/* <div className="d-none d-sm-block">
+            {isAuthenticated && !isLoading && <UserMenu />}
+          </div> */}
+
+          <div className="me-4 mb-2 ">
+            {isAuthenticated && !isLoading && <UserMenu />}
           </div>
-        </div>
-      </nav>
-    </div>
+          <div className="me-4 mb-2">
+            {isAuthenticated && !isLoading && <LogoutButton />}
+            {!isAuthenticated && !isLoading && <LoginButton />}
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 

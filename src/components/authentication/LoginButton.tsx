32522c4cc7e1ca31878f1f05e0c452d8d9a77416
Mannushka <zaimar1910@gmail.com>
2024-05-24 23:40:54 +0800

    edit log in btn

--- src/components/authentication/LoginButton.tsx
+++ src/components/authentication/LoginButton.tsx
@@ -1,10 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import { MdOutlineLogin } from "react-icons/md";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button
+      onClick={() => loginWithRedirect()}
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      Log in <MdOutlineLogin style={{ marginLeft: "5px" }} />
+    </button>
+  );
 };
 
 export default LoginButton;

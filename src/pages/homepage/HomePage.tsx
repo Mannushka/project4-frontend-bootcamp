241fcf965bc88366499b79ae929f7863db74a823
Mannushka <zaimar1910@gmail.com>
2024-05-25 23:08:40 +0800

    changes home page slides

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,18 +1,26 @@
 import ImageCarousel from "../../components/ui/imageGallery/ImageCarousel";
 import "./HomePage.css";
 import { Stack, Box, Heading } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
-import { BACKEND_URL } from "../../constants";
 import AllReviews from "../../components/review/restaurantReviews/AllReviews";
 
 const HomePage = (): JSX.Element => {
   // const [reviewPage, setReviewPage] = useState<number>(1);
   const homepafeSlides = [
+    // {
+    //   src: "https://firebasestorage.googleapis.com/v0/b/food-harbour.appspot.com/o/homepage-images%2Fparadise-dynasty.JPEG?alt=media&token=3b2597d4-be44-4ba4-ada0-2a2e6e68d7a1",
+    // },
+    // {
+    //   src: "https://media.timeout.com/images/105554269/image.jpg",
+    // },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/food-harbour.appspot.com/o/homepage-images%2Fchef.JPEG?alt=media&token=46229385-c095-44f6-8794-01a4645052c7",
+      src: "https://firebasestorage.googleapis.com/v0/b/food-harbour.appspot.com/o/homepage-images%2Fparadise-dynasty3.JPEG?alt=media&token=5d7d71f9-60a3-4669-a11d-4e4cf5084516",
+    },
+
+    {
+      src: "https://firebasestorage.googleapis.com/v0/b/food-harbour.appspot.com/o/homepage-images%2Fsummer-palace3.PNG?alt=media&token=88363b9e-1a43-489d-b4ae-1799911d5265",
     },
     {
-      src: "https://firebasestorage.googleapis.com/v0/b/food-harbour.appspot.com/o/homepage-images%2Fparadise-dynasty.JPEG?alt=media&token=3b2597d4-be44-4ba4-ada0-2a2e6e68d7a1",
+      src: "https://firebasestorage.googleapis.com/v0/b/food-harbour.appspot.com/o/homepage-images%2Flau-haa-hotpot3.JPEG?alt=media&token=efcba244-4b45-4c99-a630-b976f25201bd",
     },
   ];
   return (
@@ -24,7 +32,7 @@ const HomePage = (): JSX.Element => {
         <Heading> Recent activities</Heading>
       </Box>
       <Box>
-        <AllReviews reviewPage />
+        <AllReviews />
       </Box>
     </Stack>
   );

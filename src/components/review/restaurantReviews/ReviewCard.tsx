2db4ab20da340076d0a69b965e17d44b491c0ae7
Mannushka <zaimar1910@gmail.com>
2024-05-25 19:53:52 +0800

    add show more btn for reviews

--- src/components/review/restaurantReviews/ReviewCard.tsx
+++ src/components/review/restaurantReviews/ReviewCard.tsx
@@ -1,24 +1,43 @@
-import { Card, CardBody, Text, Box } from "@chakra-ui/react";
+import { Card, CardBody, Text, Box, Button } from "@chakra-ui/react";
 import StarRatingDisplay from "../starRating/StarRatingDisplay";
 import { formatDate } from "../../../utils/formatDate";
 import { ImageGrid } from "../../ui/imageGallery/ImageGrid";
 import { useUserInfo } from "../../../context/UserInfoContext";
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 interface ReviewCardProps {
   review: Review;
   displayResturantName: boolean;
 }
 
+const textStyle = {
+  WebkitLineClamp: 3,
+  WebkitBoxOrient: "vertical" as string,
+  overflow: "hidden",
+  display: "-webkit-box",
+};
+
 const ReviewCard = ({
   review,
   displayResturantName,
 }: ReviewCardProps): JSX.Element => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const [showReadMoreButton, setShowReadMoreButton] = useState<boolean>(false);
   const date = formatDate(review.createdAt);
   const images = review.review_photos;
   const userName = review.user.first_name;
   const restaurantName = review.restaurant.name;
   const { userId } = useUserInfo();
+
+  const ref = useRef<HTMLParagraphElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      setShowReadMoreButton(
+        ref.current.scrollHeight !== ref.current.clientHeight
+      );
+    }
+  }, []);
 
   const reviewCard = (
     <Card>
@@ -40,7 +59,24 @@ const ReviewCard = ({
         </Text>
         <Text> {date}</Text>
         <StarRatingDisplay rating={review.rating_value} />
-        <Text> {review.text}</Text>
+        <Text
+          className={
+            isExpanded ? "review-text-expanded" : "review-text-not-expanded"
+          }
+          ref={ref}
+        >
+          {review.text}
+        </Text>
+        {showReadMoreButton && (
+          <Box className="show-more-btn">
+            <Button
+              onClick={() => setIsExpanded((prev) => !prev)}
+              variant="ghost"
+            >
+              {isExpanded ? "Show less" : "Show more..."}
+            </Button>
+          </Box>
+        )}
         <ImageGrid images={images} />
       </CardBody>
     </Card>

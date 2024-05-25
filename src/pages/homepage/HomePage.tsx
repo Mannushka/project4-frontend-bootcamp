61b6fc773c53bfb348ab6343a285047d8e362756
Mannushka <zaimar1910@gmail.com>
2024-05-25 15:13:17 +0800

    create home page

---
+++ src/pages/homepage/HomePage.tsx
@@ -0,0 +1,12 @@
+import ImageCarousel from "../../components/ui/imageGallery/ImageCarousel";
+import "./HomePage.css";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <div className="home-page-container">
+      <ImageCarousel />
+    </div>
+  );
+};
+
+export default HomePage;

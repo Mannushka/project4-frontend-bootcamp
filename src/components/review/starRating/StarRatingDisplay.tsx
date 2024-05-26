e442587ce0e634d7e310fa684a1e9664c2a5de12
Mannushka <zaimar1910@gmail.com>
2024-05-26 21:13:31 +0800

    improve star rating logic

--- src/components/review/starRating/StarRatingDisplay.tsx
+++ src/components/review/starRating/StarRatingDisplay.tsx
@@ -1,15 +1,19 @@
 import { FaStar } from "react-icons/fa";
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 interface StarRatingDisplayProps {
   rating: number;
 }
 const StarRatingDisplay = ({ rating }: StarRatingDisplayProps): JSX.Element => {
-  const stars = [...Array(5)].map((_, index) => (
-    <Box key={index}>
-      <FaStar size={30} color={index < rating ? "#FFD700" : "gray"} />
-    </Box>
-  ));
+  const stars = [...Array(5)].map((_, index) => {
+    const newIndex = index + 0.5;
+    const starColor = newIndex <= rating ? "#FFD700" : "gray";
+    return (
+      <div key={index}>
+        <FaStar size={30} color={starColor} />
+      </div>
+    );
+  });
 
   return <Flex>{stars}</Flex>;
 };

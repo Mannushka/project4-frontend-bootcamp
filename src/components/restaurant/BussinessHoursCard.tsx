2ab1ce504b050d2888cd3adc1c28b832fad18480
Mannushka <zaimar1910@gmail.com>
2024-05-27 01:26:48 +0800

    edit bussiness hours display logic

--- src/components/restaurant/BussinessHoursCard.tsx
+++ src/components/restaurant/BussinessHoursCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Stack, Text, Heading } from "@chakra-ui/react";
+import { Card, CardBody, Stack, Heading } from "@chakra-ui/react";
 import { formatBusinessHours } from "../../utils/formatBusinessHours";
 
 interface BussinessHoursCardProps {
@@ -6,14 +6,21 @@ interface BussinessHoursCardProps {
 }
 
 const BussinessHoursCard = ({ restaurant }: BussinessHoursCardProps) => {
+  const bussinessHours = formatBusinessHours(restaurant.business_hours);
+  const newBussinessHours = bussinessHours.map((day, index) => (
+    <div key={index} style={{ fontSize: 15 }}>
+      {day} <br />
+    </div>
+  ));
   const card = (
     <Card variant="unstyled">
       <CardBody>
         <Stack>
           <Heading size="md">Business hours </Heading>
-          <Text style={{ fontSize: 15 }}>
+          {/* <Text style={{ fontSize: 15 }}>
             {formatBusinessHours(restaurant.business_hours)}
-          </Text>
+          </Text> */}
+          {newBussinessHours}
         </Stack>
       </CardBody>
     </Card>

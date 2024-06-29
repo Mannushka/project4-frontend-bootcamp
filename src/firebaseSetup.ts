e81ccb8c7954e39f510a65539aef3a4a2e41d2bc
Mannushka <zaimar1910@gmail.com>
2024-06-29 23:22:20 +0800

    delete unused code

--- src/firebaseSetup.ts
+++ src/firebaseSetup.ts
@@ -2,9 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
